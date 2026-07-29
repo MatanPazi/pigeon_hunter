ac4cf1be0d1a6939f126c9faa3a7c4a5d71b7ef3
MatanP <matanpaziyt@gmail.com>
2026-07-29 17:49:06 +0300

    reverted image storing file

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ DATA_DIR = BASE_DIR / "data"
 
 # ========================= CONFIG =========================
 BASE_DIR = Path(__file__).resolve().parents[1]
-DATA_DIR = BASE_DIR / "data/false_detection"
+DATA_DIR = BASE_DIR / "data"
 DATA_DIR.mkdir(exist_ok=True)
 
 THRESHOLD = 100
